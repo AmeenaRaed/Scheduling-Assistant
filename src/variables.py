ffc1c76c095c9ded9da299f3e15734672d554bd9
ameena221 <ameenaraed221@gmail.com>
2025-12-06 21:10:40 +0300

    Variables added

---
+++ src/variables.py
@@ -0,0 +1,90 @@
+courses = ["AI", "Web Development", "Data Science", "Algorithms",
+           "Cloud Computing", "Mobile Development", "Internet of Things",
+           "DevOps", "Operating Systems", "UI/UX Design"]
+
+instructors = ["Dr. Nabil",
+               "Dr. Fatima",
+               "Dr. Amal",
+               "Dr. Ali",
+               "Dr. Qasem",
+               "Dr. Hadeel",
+               "Dr. Taher", 
+               "Dr. Mohammed", 
+               "Dr. Abdulla",
+               "Dr. Manal"]
+
+course_instructor = {
+    "AI": "Dr. Nabil",
+    "Web Development": "Dr. Abdulla",
+    "Data Science": "Dr. Amal",
+    "Algorithms": "Dr. Ali",
+    "Cloud Computing": "Dr. Amal",
+    "Mobile Development": "Dr. Manal",
+    "Internet of Things": "Dr. Mohammed",
+    "DevOps": "Dr. Taher",
+    "Operating Systems": "Dr. Qasem",
+    "UI/UX Design": "Dr. Hadeel"
+}
+
+# Available time slots
+domains = {
+    "AI": [
+        "Mon 8:00-9:15",      # Overlaps with DevOps
+        "Tue 10:00-10:50",    # Overlaps with Cloud Computing
+        "Wed 11:00-12:15"
+    ],
+
+    "Web Development": [
+        "Sun 9:00-9:50",      
+        "Mon 11:00-12:15",    # Overlaps with UI/UX Design
+        "Thu 12:00-12:50"
+    ],
+
+    "Data Science": [
+        "Tue 8:00-8:50",
+        "Wed 9:30-10:45",
+        "Thu 10:00-10:50"     # Overlaps with Cloud Computing
+    ],
+
+    "Algorithms": [
+        "Mon 9:30-10:45",
+        "Wed 12:30-13:45",
+        "Thu 9:00-9:50"       # Overlaps with Mobile Development
+    ],
+
+    "Cloud Computing": [
+        "Wed 8:00-9:15",
+        "Thu 9:00-9:50",      
+        "Tue 10:00-10:50"     # Overlaps with AI
+    ],
+
+    "Mobile Development": [
+        "Mon 9:30-10:45",
+        "Wed 12:30-13:45",
+        "Thu 9:00-9:50"       # Overlaps with Algorithms
+    ],
+
+    "Internet of Things": [
+        "Sun 11:00-11:50",
+        "Tue 9:00-9:50",
+        "Wed 11:00-12:15"     # Overlaps with AI
+    ],
+
+    "DevOps": [
+        "Mon 8:00-9:15",      # Overlaps with AI
+        "Thu 11:00-11:50",
+        "Tue 9:00-9:50"       # Overlaps with Internet of Things
+    ],
+
+    "Operating Systems": [
+        "Wed 9:30-10:45",     # Overlaps with Data Science
+        "Thu 10:00-10:50",
+        "Sun 12:00-12:50"
+    ],
+
+    "UI/UX Design": [
+        "Sun 8:00-8:50",
+        "Tue 12:00-12:50",
+        "Mon 11:00-12:15"     # Overlaps with Web Development
+    ]
+}
